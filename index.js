62b20d484863de3d6644139e571a01a39ba81149
Afzaal Khan <162747532+afzaalkhan27@users.noreply.github.com>
2024-03-26 23:53:20 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter your first number", type: "number", name: "firstNumber" },
+    { message: "Enter your second number", type: "number", name: "secondNumber" },
+    {
+        message: "select one of operators to perform action ",
+        type: "list",
+        name: "operators",
+        choices: ["ADDITION", "SUBTRACTION", "MULTIPLICATION", "DIVISION"],
+    },
+]);
+if (answer.operators === "ADDITION") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operators === "SUBTRACTION") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operators === "MULTIPLICATION") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operators === "DIVISION") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    "Please select correct operator";
+}
